2b40b65d91d600963720a70f6fab2748c1bf362a
Alexandr <Konsolka@users.noreply.github.com>
2019-11-20 14:49:07 +0300

    woking s

--- srcs/print_s.c
+++ srcs/print_s.c
@@ -6,35 +6,50 @@
 /*   By: mburl <mburl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 12:26:39 by abenton           #+#    #+#             */
-/*   Updated: 2019/11/15 15:49:01 by mburl            ###   ########.fr       */
+/*   Updated: 2019/11/18 16:31:29 by mburl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char	*s2;
+	size_t	i;
+
+	i = 0;
+	if (!(s2 = ft_strnew(n)))
+		return (NULL);
+	while (s1[i] && i < n)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	return (s2);
+}
+
 int		print_s(t_flags	*flags, va_list args)
 {
 	char	*s;
 	int		len;
+	int		width;
 
 	s = va_arg(args, char *);
-//	if (flags->len == LEN_TYPE_L)
-//		s = (wchar_t *)s;
-//	else
-//		s = (char *)s;
-	if (flags->precision > 0 && s)
-		s = strndup(flags->precision, s);	//FT_STRDUP
-	else if (flags->precision == 0 && s)
-		s = ft_strdup(s);
-	else if (flags->precision > 0 && !s)
-		s = strndup("(null)", flags->precision); // FT_STRDUP
-	else if (flags->precision == 0 && !s)
+	if (!s)
 		s = ft_strdup("(null)");
-	len = ft_strlen(s);
-	if (flags->width && !flags->minus)
-		len += ft_pad(flags, len);
-	ft_write(s, (int)ft_strlen(s), flags);
-	if (flags->width && flags->minus)
-		len += ft_pad(flags, len);
-	return ((int)ft_strlen(s));
+	len = (int)ft_strlen(s);
+	len = (flags->precision == -1) ? 0 : len;
+	if (flags->precision > 0 && flags->precision < len && len > 0)
+		len = flags->precision;
+	if (flags->width)
+	{
+		if (flags->minus)
+			ft_write(s, len, flags);
+		width = 0;
+		while (width++ < flags->width - len)
+			ft_write((flags->zero && !flags->minus) ? "0" : " ", 1, flags);
+	}
+	if (!flags->width || !flags->minus)
+		ft_write((s) ? s : "(null)", len, flags);
+	return (len + (flags->width - len > 0 ? flags->width - len : 0));
 }
