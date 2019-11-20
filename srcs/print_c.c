84f66f12c0c15f5c04f62a3c2a699a4995a12575
Anivia Benton <abenton@mi-p5.21-school.ru>
2019-11-20 17:26:45 +0300

    print c

--- srcs/print_c.c
+++ srcs/print_c.c
@@ -3,34 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   print_c.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abenton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: abenton <abenton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/19 12:02:04 by abenton           #+#    #+#             */
-/*   Updated: 2019/11/19 12:46:43 by abenton          ###   ########.fr       */
+/*   Created: 2019/11/20 16:49:01 by abenton           #+#    #+#             */
+/*   Updated: 2019/11/20 17:26:32 by abenton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		print_c(t_flags *flags, va_list args)
+int     print_c(t_flags	*flags, va_list args)
 {
-	wint_t	c;
-	
-	if (flags->len == LEN_TYPE_L)
-		c = (unsigned long)va_arg(args, unsigned long);
-/*
- * Обработка большой буквы!
- 	else if ()
-		c = (wchar_t)va_arg(tab->args, wint_t);
-*/
-	else
-		c = (char)va_arg(args, int);
-	if (flags->zero && !flags->minus)
-		kek;
-	else if (!flags->minus)
-		kek1;
-	ft_write();
-	if(flags->minus)
-		kek2;
-	return (1 + (flags->width - 1 > 0 ? flags->width - 1 : 0));
+    int     c;
+    int     width;
+
+    width = 0;
+    c = (char)va_arg(args, int);
+    if (flags->width)
+    {
+        if (flags->minus)
+            ft_write(&c, 1, flags);
+        while (width++ < flags->width - 1)
+            ft_write((flags->zero && !flags->minus) ? "0" : " ", 1, flags);
+    }
+    if (!flags->width || !flags->minus)
+		ft_write(&c, 1, flags);
+    if (flags->width)
+        return (width);
+    else
+        return (1);
 }
